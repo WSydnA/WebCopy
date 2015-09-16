3ccf3603d8543cc00ca9c97a90ed17255117e20d
Andy White <andywhite87@gmail.com>
2015-09-16 23:48:56 +0100

    Minor updates to file names and folders

--- gruntfile.js
+++ gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
         footer: '\n})();\n'
       },
       dist: {
-        src: ['<%=s%>lib/_cuts-the-mustard.js', '<%=s%>_mustard-cut.js', '<%=s%>lib/*.js',
+        src: ['<%=s%>lib/cuts-the-mustard.js', '<%=s%>_mustard-cut.js', '<%=s%>lib/*.js',
               '<%= s %>_jsdoc.js', '<%=s%>main.js', '<%=s%>_footer.js'],
         dest: 'dist/<%= filename %>.js'
       }
@@ -40,7 +40,7 @@ module.exports = function(grunt) {
     jasmine: {
       src: ['<%=s%>lib/*.js', '<%=s%>main.js'],
       options: {
-        specs: '<%=t%>lib/*.js'
+        specs: '<%=t%>/*.js'
       }
     },
 
