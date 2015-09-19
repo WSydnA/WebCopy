21042350f5834af95c5c11f81cd7a427647ecf26
Andy White <andywhite87@gmail.com>
2015-09-19 21:28:17 +0100

    Added unit test for WebCopy's return value in unsupported browsers

--- test/cuts-the-mustard.tests.js
+++ test/cuts-the-mustard.tests.js
@@ -14,6 +14,23 @@ describe("cutsTheMustard", function() {
     var cuts = cutsTheMustard();
 
     expect(cuts).not.toBeTruthy();
+    expect(WebCopy.isSupported).toBe(false);
+
+  });
+
+  it("creates a constructor that returns invisible elements in old browsers", function() {
+
+    window.addEventListener = undefined;
+    var cuts = cutsTheMustard();
+
+    var testButton = new WebCopy();
+    var classes = testButton.classList.toString();
+    var displayStyle = testButton.style.display;
+
+    expect(cuts).not.toBeTruthy();
+    expect(WebCopy.isSupported).toBe(false);
+    expect(classes).toBe("webCopy-not-supported");
+    expect(displayStyle).toBe("none");
 
   });
 
