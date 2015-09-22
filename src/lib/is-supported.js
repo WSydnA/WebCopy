9160419d79af91fc8c9773ea2de9df16802d3994
Andy White <andywhite87@gmail.com>
2015-09-22 22:12:45 +0100

    Updated use of arguments in isSupported() for consistency with other methods

--- src/lib/is-supported.js
+++ src/lib/is-supported.js
@@ -1,23 +1,26 @@
-  var isSupported = function(navigator, document) {
+  var isSupported = function(nav, doc) {
+
+    nav = nav || navigator;
+    doc = doc || document;
 
     // Support isn't reported correctly on Safari, so UA sniffing is used to discount Safari on desktop or iDevices
     // (it's likely to be a while before they support this)
-    var isSafari = (navigator.userAgent.indexOf("Safari") > -1 || navigator.userAgent.indexOf("AppleWebKit") > -1) &&
-        navigator.userAgent.indexOf("Chrome") < 0;
+    var isSafari = (nav.userAgent.indexOf("Safari") > -1 || nav.userAgent.indexOf("AppleWebKit") > -1) &&
+        nav.userAgent.indexOf("Chrome") < 0;
     if (isSafari) {
       return false;
     }
 
     // Checking for support in advance triggers a permissions pop up in supported versions of IE
     // We want to avoid this at least until the button is clicked, so bypass this check in IE > 8
-    var isIE = navigator.userAgent.indexOf("MSIE ") > -1 || navigator.userAgent.indexOf("Trident/") > -1;
+    var isIE = nav.userAgent.indexOf("MSIE ") > -1 || nav.userAgent.indexOf("Trident/") > -1;
     if (isIE) {
       return true;
     }
 
     // Other supported browsers return a boolean in response to the following query
     try {
-      if (typeof document.execCommand("copy") === "boolean") {
+      if (typeof doc.execCommand("copy") === "boolean") {
         return true;
       }
     }
